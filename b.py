40c4f7f0f76c56ba6e8d5755b344bbc0d27a8e38
Diogo José Bento <100057646+dbentooojj@users.noreply.github.com>
2022-06-11 22:23:19 -0300

    1

--- b.py
+++ b.py
@@ -21,9 +21,6 @@ lista_senhas = []
 nome = []
 sg.theme('Reddit')
 
-
-# Criar as janelas e estilos (layout)
-
 def janela_login():
     sg.theme('Reddit')
     layout = [
@@ -36,14 +33,13 @@ def janela_login():
         [sg.Checkbox('Salvar Login?', key='lembrar', font=("bold", 15))],
         [sg.Button('Entrar', font=("bold", 15)), sg.Button('Cadastrar', font=("bold", 15))],
         [sg.Text('', size=(10, 1), key='mensagem')],
-
     ]
 
     return sg.Window('Iniciar sessão no entra21', layout=layout, finalize=True)
 
 image = [
     [sg.Image(key="-IMAGE-")],
-]
+    ]
 
 def janela_cadastro():
     sg.theme('Reddit')
@@ -51,7 +47,7 @@ def janela_cadastro():
 
         [sg.Push(), sg.Column(image), sg.Push()],
         [sg.FileBrowse('Procurar Img', file_types=file_types, key="-FILE-", size=(30, 1)),
-         sg.Button("Load Image", size=(29, 1))],
+            sg.Button("Load Image", size=(29, 1))],
         [sg.Text('Primeiro nome', font=("bold", 15))],
         [sg.Input(key='primeiro_nome', font=("bold", 15))],
         [sg.Text('Ultimo nome', font=("bold", 15))],
@@ -69,18 +65,16 @@ def janela_cadastro():
 
     return sg.Window('Cadastro', layout=layout, finalize=True)
 
-
 end1 = [
     
     [sg.Text('CEP', font=("bold", 15)), sg.InputText(key='cep', size=(15, 1), font=("bold", 15)), sg.Button('Consultar', font=("bold", 15))],
     [sg.Text('Logradouro', font=("bold", 15)) ,sg.InputText(key='logradouro', size=(28, 1),font=("bold", 15)), sg.Text('Número', font=("bold", 15)), sg.InputText(key='numero', size=(8, 1), font=("bold", 15)), sg.Text('UF', font=("bold", 15)), sg.InputText(key='uf', size=(7, 1), font=("bold", 15))],
     [sg.Text('Bairro', font=("bold", 15)), sg.InputText(key='bairro', size=(23, 1), font=("bold", 15)), sg.Text('Cidade', font=("bold", 15)), sg.InputText(key='cidadee', size=(23, 1), font=("bold", 15))], 
     
-]
+    ]
 
 def janela_3():
     sg.theme('Reddit')
-    
     layout = [
         
         [sg.Push(),sg.Image(filename='aa.png'), sg.Push()],
@@ -89,31 +83,28 @@ def janela_3():
         [sg.Text(key='nome')],
 
         [sg.Text('Data de Nasc.', font=("bold", 15)),
-        sg.Combo(lista_dia, key='dia', font=("bold", 15), size=(5, 1)),
-        sg.Text('/'),
-        sg.Combo(lista_mes, key='mes', font=("bold", 15), size=(5, 1)),
-        sg.Text('/'),
-        sg.Combo(lista_ano, key='ano', font=("bold", 15), size=(5, 1)),
-        sg.Text('Sexo', font=("bold", 15)),
-        sg.Combo(('Masculino', 'Feminino'), key='sexo', size=(10, 1), font=("bold", 15))],
+            sg.Combo(lista_dia, key='dia', font=("bold", 15), size=(5, 1)),
+            sg.Text('/'),
+            sg.Combo(lista_mes, key='mes', font=("bold", 15), size=(5, 1)),
+            sg.Text('/'),
+            sg.Combo(lista_ano, key='ano', font=("bold", 15), size=(5, 1)),
+            sg.Text('Sexo', font=("bold", 15)),
+            sg.Combo(('Masculino', 'Feminino'), key='sexo', size=(10, 1), font=("bold", 15))],
 
         [sg.Text('Telefone', font=("bold", 15)), sg.InputText(key='telefone', size=(21, 1), font=("bold", 15)),
-        sg.Text('Celular', font=("bold", 15)), sg.InputText(key='celular', size=(22, 1), font=("bold", 15))],
+            sg.Text('Celular', font=("bold", 15)), sg.InputText(key='celular', size=(22, 1), font=("bold", 15))],
 
         [sg.Column(end1, element_justification='center')],
-        
         [sg.Button("Voltar", font=("bold", 15)), sg.Button("Salvar", font=("bold", 15))],
     ]
 
     return sg.Window("Seu perfil", layout=layout, finalize=True, element_justification='center')
 
-
 janela1, janela2, janela3 = janela_login(), None, None
 
-# Criar um loop de leitura de eventos
 while True:
     window, event, values = sg.read_all_windows()
-    # Quando a janela for fechada
+ 
     if window == janela1 and event == sg.WIN_CLOSED:
         break
     elif window == janela2 and event == sg.WIN_CLOSED:
@@ -121,11 +112,9 @@ while True:
     elif window == janela3 and event == sg.WIN_CLOSED:
         break
 
-    # Quando o botão de cadastro for clicado
     if window == janela1 and event == 'Cadastrar':
         janela1.hide()
         janela2 = janela_cadastro()
-    # Quando o botão de voltar for clicado
     if window == janela2 and event == 'Voltar':
         janela2.close()
         janela1 = janela_login()
@@ -140,17 +129,12 @@ while True:
             image = Image.open(values["-FILE-"])
             image.thumbnail((200, 200), Image.ANTIALIAS)
             bio = io.BytesIO()
-            # Actually store the image in memory in binary
             image.save(bio, format="PNG")
-            # Use that image data in order to
             window["-IMAGE-"].update(data=bio.getvalue())
 
     if window == janela2 and event == 'Salvar':
         if values["senha"] == values["confirmar"]:
 
-            
-            nome.append(values["primeiro_nome"])
-            nome.append(values["ultimo_nome"])
             lista_usuarios.append(values['email'])
             lista_senhas.append(values['senha'])
             sg.popup("Cadastrado com sucesso", font=("bold", 15))
@@ -159,25 +143,19 @@ while True:
         else:
             sg.popup("As senhas não coincidem", font=("bold", 15))
 
-
     if window == janela1 and event == 'Entrar':
         if values["email"] in lista_usuarios:
             try:
                 if lista_usuarios.index(values["email"]) == lista_senhas.index(values["senha"]):
                     janela1.close()
                     janela3 = janela_3()
-                    janela3['nome'].update(nome[1], nome[1])
-                    
-
-                    
+                       
                 elif lista_usuarios.index(values["email"]) != lista_senhas.index(values["senha"]):
                     sg.popup("Senha incorreta", font=("bold", 15))
                     
-                
             except:
                 pass
                 
-
         else:
             sg.popup("Email não cadastrado", font=("bold", 15))
 
@@ -190,6 +168,4 @@ while True:
         janela3.Element('bairro').Update(response_json['bairro'])
         janela3.Element('uf').Update(response_json['uf'])
         janela3.Element('cidadee').Update(response_json['localidade'])
-        print(response)
-        print(response_json)
         
